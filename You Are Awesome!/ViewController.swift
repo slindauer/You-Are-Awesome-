39fdd94d17b4166c66bccfd7d31cf0c70628c911
Steven Lindauer <lindaues@bellsouth.net>
2021-07-18 17:01:58 -0400

    Section 2.6 - Revert to single button, add imageViewer and several images to assets

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -9,26 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var imageViewer: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("😜 viewDidLoad() did indeed load.")
         messageLabel.text = "Startup complete!"
     }
 
-
-    @IBAction func secondMessageButtonPressed(_ sender: UIButton) {
-        messageLabel.text = "2nd Message!"
-        messageLabel.textColor = .red
-        messageLabel.textAlignment = .right
-    }
     
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        print("😎 The message button was pressed!")
         messageLabel.text = "You are awesome!"
-        messageLabel.textColor = .blue
-        messageLabel.textAlignment = .left
+        imageViewer.image = UIImage(named: "image0")
     }
 }
 
