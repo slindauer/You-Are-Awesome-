a0fbfc8c4b00e8dd777bcfd703f97c7a74d91317
Steven Lindauer <lindaues@bellsouth.net>
2021-07-17 23:26:19 -0400

    Initial commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Steven Lindauer on 7/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ You Are Awesome!Tests/You_Are_Awesome_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  You_Are_Awesome_Tests.swift
+//  You Are Awesome!Tests
+//
+//  Created by Steven Lindauer on 7/17/21.
+//
+
+import XCTest
+@testable import You_Are_Awesome_
+
+class You_Are_Awesome_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
