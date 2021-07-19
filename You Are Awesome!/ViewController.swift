58cd9ed0a3c0707084164d328ff8f49d1c18e9c5
Steven Lindauer <lindaues@bellsouth.net>
2021-07-19 13:45:35 -0400

    Section 2.9 - cycling through 10 images on button press

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageViewer: UIImageView!
     
+    var imageNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -19,22 +21,30 @@ class ViewController: UIViewController {
 
     
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        let awesomeMessage = "You are awesome!"
-        let greatMessage = "You are great!"
-        let dabombMessage = "You are da bomb!"
-        
-        if messageLabel.text == awesomeMessage {
-            messageLabel.text = greatMessage
-            imageViewer.image = UIImage(named: "image1")
-            
-        } else if messageLabel.text == greatMessage {
-            messageLabel.text = dabombMessage
-            imageViewer.image = UIImage(named: "image2")
-            
-        } else {
-            messageLabel.text = awesomeMessage
-            imageViewer.image = UIImage(named: "image0")
+//        let imageName = "image" + String(imageNumber)
+        let imageName = "image\(imageNumber)"
+        imageViewer.image = UIImage(named: imageName)
+        imageNumber += 1
+        if(imageNumber > 9){
+            imageNumber = 0
         }
+        
+        //        let awesomeMessage = "You are awesome!"
+        //        let greatMessage = "You are great!"
+        //        let dabombMessage = "You are da bomb!"
+        //
+        //        if messageLabel.text == awesomeMessage {
+        //            messageLabel.text = greatMessage
+        //            imageViewer.image = UIImage(named: "image1")
+        //
+        //        } else if messageLabel.text == greatMessage {
+        //            messageLabel.text = dabombMessage
+        //            imageViewer.image = UIImage(named: "image2")
+        //
+        //        } else {
+        //            messageLabel.text = awesomeMessage
+        //            imageViewer.image = UIImage(named: "image0")
+        //        }
         
     }
 }
